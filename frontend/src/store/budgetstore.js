5ae6ede019c9f79677a8ce4aeb1f9b222d765690
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-03-16 21:59:59 +0530

    Update budgetstore.js

--- frontend/src/store/budgetstore.js
+++ frontend/src/store/budgetstore.js
@@ -11,7 +11,7 @@ export const useBudgetStore = create ((set, get) => ({
     getBudget: async () => {
         set({isBudgetLoading: true});
         try {
-            const response = await axios.get('http://localhost:5000/api/budget', {withCredentials: true});
+            const response = await axios.get('https://personal-finance-visualizer-i4xy.onrender.com/api/budget', {withCredentials: true});
             set({budgets: response.data.budgets});
         } catch (error) {
             toast.error('Failed to fetch Budgets');
@@ -21,7 +21,7 @@ export const useBudgetStore = create ((set, get) => ({
     addBudget: async(data) => {
         set({isBudgetAdding: true});
         try {
-            const response = await axios.post('http://localhost:5000/api/budget/add', data, {withCredentials: true});
+            const response = await axios.post('https://personal-finance-visualizer-i4xy.onrender.com/api/budget/add', data, {withCredentials: true});
             set({budgets: [...get().budgets, response.data.budget]});
             toast.success('Budget added successfully');
         } catch (error) {
@@ -32,7 +32,7 @@ export const useBudgetStore = create ((set, get) => ({
     deleteBudget: async() => {
         set({isBudgetDeleting: true});
         try {
-            await axios.delete(`http://localhost:5000/api/budget/delete`, {withCredentials: true});
+            await axios.delete(`https://personal-finance-visualizer-i4xy.onrender.com/api/budget/delete`, {withCredentials: true});
             set({budgets: []});
             toast.success('Budget deleted successfully');
         } catch (error) {
